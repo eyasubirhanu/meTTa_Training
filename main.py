5fb36b49164b3e6e931e894da580ffec3ed919ab
amanuel-1 <amanuelgaromsa@gmail.com>
2024-08-17 13:11:50 +0300

    feat: added logic

--- main.py
+++ main.py
@@ -30,19 +30,49 @@ except Exception as e:
 
 # 2 point
 def get_transcript(node):
-    transcript = metta.run() #TODO
+    transcript = metta.run(f'''
+                           !(match &space (transcribed_to ({node[0]}) $transcript) (transcribed_to ({node[0]}) $transcript))
+                           '''
+                           ) #TODO
+    # print("transcript ---> ",test)
     return transcript     #[[(, (transcribed_to (gene ENSG00000175793) (transcript ENST00000339276)))]]
 
 #2 point
 def get_protein(node):
-    protein = metta.run() #TODO
+    protein = metta.run(f'''
+                           !(let $transcript
+                            (match &space (transcribed_to ({node[0]}) $transcript) $transcript)
+                             (match &space (translates_to $transcript $protein) (translates_to $transcript $protein) )
+
+)
+                        ''') #TODO
     return protein
 
-def metta_seralizer(metta_result):
+def metta_seralizer(metta_result):#in the form of [output1 , output 2]
     #TODO
+    result = []
+    for output in metta_result:
+        result =[]
+        for ex_atom in output:
+            # solo_result = solo_result.replace('(','').replace(')','').split(' ')
+            # print(solo_result.get_children())
+            
+            solo_result= repr(ex_atom.get_children()).replace('(','').replace(')','').replace('[','').replace(']','').split(',')
+            # solo_result = repr(solo_result).split('\n')
+            # print("splitted ===== ",(repr(solo_result)))
+            result.append({
+                'edge':solo_result[0],
+                'source':solo_result[1],
+                'target':solo_result[2]
+            }) 
+            # print(splitted)
+            print("ended")
+            
+            
+            
     return result
 
-result= (get_transcript(['gene ENSG00000175793'])) # change the gene id to "ENSG00000166913"
+result= (get_transcript(['gene ENSG00000166913'])) # change the gene id to "ENSG00000166913"
 print(result) #[[(, (transcribed_to (gene ENSG00000175793) (transcript ENST00000339276)))]]
 
 #6 point
